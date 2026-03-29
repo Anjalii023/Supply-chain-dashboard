3200b7b8269674ca783b7286857526dd4fa27936
Anjali Bhat <anjalibhat235@gmail.com>
2026-03-29 21:41:41 +0530

    Initial commit - supply chain dashboard

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+const Index = () => {
+  return (
+    <iframe
+      src="/supply_chain_dashboard.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+      title="Supply Chain Analytics Lab"
+    />
+  );
+};
+
+export default Index;
